1c63d4d777a2552404a36d1fa26c1d986822f4f7
DonaverBoxwood <donaver.boxwood.personal@gmail.com>
2022-12-14 08:49:47 -0500

    Initial commit

---
+++ src/main/java/net/boxwood/ecodeco/EcoDeco.java
@@ -0,0 +1,51 @@
+package net.boxwood.ecodeco;
+
+// NOTES TO SELF
+// to make this into a .jar file: open the Gradle
+// sidebar, and use the "build" option inside the "build"
+// folder (which is named rather redundantly but whatever)
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(EcoDeco.MOD_ID)
+public class EcoDeco {
+    public static final String MOD_ID = "ecodeco";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public EcoDeco() {
+        IEventBus modEventBus =
+                FMLJavaModLoadingContext.get().getModEventBus();
+
+        // Register the commonSetup method for modloading
+        modEventBus.addListener(this::commonSetup);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+
+    }
+
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
+
+        }
+    }
+}
